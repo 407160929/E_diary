d2e95c361289851206afcd07920433245b6181f9
Mr.boo <407160929@qq.com>
2023-11-22 19:16:53 +0800

    first commit

---
+++ src/main/java/com/group/e_diary/EDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.group.e_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EDiaryApplication.class, args);
+	}
+
+}
